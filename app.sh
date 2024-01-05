0cacca4d74676b0a331178dbd7255b9a2bbd5652
jyeung79 <jyeung.eng@gmail.com>
2024-01-04 16:54:12 -0800

    feat: Update casks and apps

--- app.sh
+++ app.sh
@@ -14,37 +14,26 @@ EOS
 echo " ----- Install web apps ------"
 sudo softwareupdate --install-rosetta
 
-brew install --cask 1password
+brew install --cask bitwarden
+brew install --cask iterm2
 brew tap AdoptOpenJDK/openjdk
 brew install --cask adoptopenjdk
 brew install --cask android-studio
+brew install --cask visual-studio-code
 brew install --cask docker
 brew install --cask google-chrome
-brew install --cask iterm2
-brew install --cask google-japanese-ime
-brew install --cask slack
-brew install --cask spotify
+brew install --cask flipper
 brew install --cask imageoptim
-brew install --cask visual-studio-code
-brew install --cask adobe-creative-cloud
-brew install --cask gyazo
-brew install --cask zoom
+brew install --cask dbeaver-community
 brew install --cask discord
-brew install --cask notion
 brew install --cask figma
-brew install --cask tableplus
 brew install --cask postman
-brew install --cask clipy
+brew install --cask proxyman
 brew install --cask alfred
-
-read -p 'Do you want to install license need apps ? [y/n]' input
-case $input in
-'' | [Nn]*)
-  echo "Skip"
-  ;;
-[Yy]*)
-  brew install --cask microsoft-office
-  brew install --cask intellij-idea
-  brew install --cask clip-studio-paint
-  ;;
-esac
+brew install --cask vlc
+brew install ---cask nordvpn
+brew install --cask obsidian
+brew install --cask todoist
+brew tap dimentium/autoraise
+brew install --cask autoraise
+brew services start autoraise
--- appstore.sh
+++ appstore.sh
@@ -17,11 +17,22 @@ mas install 409183694  # Keynote
 mas install 425424353  # The Unarchiver
 mas install 409201541  # Pages
 mas install 1024640650 # CotEditor
-mas install 453164367  # SystemPal
-mas install 412529613  # Cinch
 mas install 409203825  # Numbers
 mas install 549083868  # Display Menu
 mas install 1549308269 # OctoLinker
 mas install 1457450145 # Octotree
 mas install 1289197285 # MindNode
+mas install 497799835 # Xcode
+mas install 1478757472 # bluebird
+mas install 634148309 # logic pro
+mas install 434290957 # motion
+mas install 424389933 # final cut pro
+mas install 424390742 # compressor
+mas install 634159523 # mainstage
+mas install 640199958 # apple developer
+mas install 899247664 # apple testflight
+mas install 1606941598 # affinity 2 publisher
+mas install 1616822987 # affinity 2 photo
+mas install 1616831348 # affinity 2 designer
+mas install 1193539993 # ibrother print and scan
 echo " ------------ END ------------"
